86e0053bacd5d256f801ba067043ccdf1b5f3c33
A Sahu <s.achyutananda6@gmail.com>
2026-02-21 16:30:01 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
-"""Hello World application."""
-
-
-def hello():
-    """Print Hello World message."""
-    print("Hello, World!")
-
-
-if __name__ == "__main__":
-    hello()
+print("Hello, World!")
